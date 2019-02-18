c3d2778020c6447870ecfb4dc08748b9da212e57
corneliuze <falaycornelius@gmail.com>
2019-02-18 11:47:50 +0100

    fixed changes

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const config = require('.config/config.json');
 
-require('dotenv').config()
+import {} from 'dotenv/config';
 
-var PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-var app = express()
-app.use(logger('dev'))
-app.use(bodyParser.urlencoded({extended:true}))
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) =>{
- return  res.send('This is an ussd app that conducts a simple survey');
-})
+ return  res.send(config.responses.baseUrlResponse);
+});
 
 app.post('/', (req, res) =>{
     let{sessionId, serviceCode, msisdn, text } = req.body;
 
     if (text == ''){
-        let response = 'CON  do you prefer rice to beans \n 1. yes \n 2. no '
-       
-        res.send(response);
+       res.send(config.responses.noReqString);
     }
     else if(text == '1'){
         let response = 'CON which one do you prefer?  \n 1. white rice and stew \n 2. jollof rice  \n  3. fried rice'
-        res.send(response)
+        res.send(config.responses.one);
     }
     else if(text == '1*1'){
         let response = 'CON do you like it with plantain, beans and chicken? \n 1. none of the above\n 2. plantain and chicken \n 3. beans and chicken\n 4. plantain beans and chicken'
-        res.send(response)
+        res.send(config.responses.one*one);
     }
 
     else if(text == '1*1*1' || '1*1*2' || '1*1*3' || '1*1*4'){
-        let response = 'END thank you for your input.'
-        res.send(response)
+    
+        res.send(config.responses.one*two*one);
     }
     else if(text == '1*2'){
         let response = 'CON do you like it with coleslaw and chicken? \n 1. none of the above \n2. coleslaw and chicken    \n 3. just chicken '
         
-        res.send(response)        
+        res.send(config.responses.one*two);       
     }
 
     else if(text == '1*2*1' || '1*2*2' || '1*2*3'){
-        let response = 'END thank you for your input.'
-        res.send(response)
+        
+        res.send(config.responses.one*two*one);
     }
     else if(text == '1*3'){
-        let response = 'CON do you like it with coleslaw, jollof rice and chicken?'
-        '1. none of the above \n 2. coleslaw and chicken \n 3. just chicken \n 4. jollof rice and chicken \n 5. all of the above'
     
-        res.send(response)        
+        res.send(config.responses.one*three);        
     }
 
     else if(text == '1*3*1' || '1*3*2' || '1*3*3' || '1*3*4' || '1*3*5'){
-        let response = ' END thank you for your input.'
-        res.send(response)
+       
+        res.send(config.responses.one*two*one);
     }
     else if(text == '2'){
-      let response = ' CON What do you like your beans with?\n 1. plantain \n 2. bread'
-         res.send(response);
+         res.send(config.responses.two);
     }
     else if( text == '2 *1' || '2*2'){
-        let response = ' END thank you for your input.'
-        res.send(response)
+       
+        res.send(config.responses.one*two*one);
         }else{
             res.status(400).send('Bad request!');
         }
@@ -70,5 +66,5 @@ app.post('/', (req, res) =>{
 
 })
 app.listen(PORT , () =>{
-    console.log("server running on port " + PORT);
+    console.log("server running on port ${PORT}");
 })
